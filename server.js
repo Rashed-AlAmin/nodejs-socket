9853cb3dc6010f90da84b600af4b20bdea47ea4d
Rashed <rashedalamin69@gmail.com>
2025-10-31 21:58:02 +0600

    created basic socket structure and some bidirectional query

---
+++ server.js
@@ -0,0 +1,33 @@
+const express=require('express')
+const http=require('http')
+const socketIo=require('socket.io')
+
+const app=express()
+
+
+const server=http.createServer(app)
+
+//initiate socket.io with http server
+const io=socketIo(server)
+//serve the static file from the public directory
+app.use(express.static('public'))
+
+const users=new Set()
+
+io.on('connection',(socket)=>{
+    console.log('a user is now connected')
+    //handle users when they will join the chat
+    socket.on('join',(userName)=>{
+        users.add(userName)
+
+        //broadcast to all users that a new user has joined
+        io.emit('userJoined',userName)
+        //send the updated userlist to all the clients
+        io.emit('userList',Array.from(users))
+        
+    })
+
+    //handle incoming chat messages
+
+    //handle user disconnection
+})
